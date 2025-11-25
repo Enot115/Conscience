4565443dc48211c5d69b5a4ffc498d81339b810c
Марченко Евгений <zeka181106@gmail.com>
2025-11-26 01:08:23 +0300

    update menu
    Add scripts for menu

--- Conscience/Assets/MainMenu.cs
+++ Conscience/Assets/MainMenu.cs
@@ -2,15 +2,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void Start()
     {
-        
+        //SceneManager.LoadScene("level1");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void ExitGame()
+    { 
+        Application.Quit();
     }
 }
