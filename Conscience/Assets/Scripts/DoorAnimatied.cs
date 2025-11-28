d7ca9b2b0b45d094d478eb49e8e7aeed43e8466b
tuxoxoq <timrus12@gmail.com>
2025-11-28 20:33:46 +0300

    Add movement player, camera, animation door1

---
+++ Conscience/Assets/Scripts/DoorAnimatied.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class DoorAnimated : MonoBehaviour
+{
+    public Animator animator;
+    private bool opened = false;
+
+    void Awake()
+    {
+        if (animator == null)
+        {
+            animator = GetComponent<Animator>();
+        }
+    }
+
+    public void OnInteract()
+    {
+        if (opened) return;   // если нужно открыть только один раз
+        opened = true;
+        if (animator != null)
+        {
+            animator.Play("DoorOpen",0,0f);
+            //animator.SetTrigger("Open");
+            Debug.Log("Door: Open triggered" + name);
+        }
+        else
+        {
+            Debug.LogWarning("DoorAnimated: нет Animator на " + name);
+        }
+    }
+}
